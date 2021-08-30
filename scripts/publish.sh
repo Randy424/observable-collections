eb12f467de91e4699ed77102f38e36fa061d401c
James Talton <jtalton@redhat.com>
2021-08-30 12:23:45 -0400

    github action
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,7 +13,7 @@ echo Publish: Checking for changes...
 PACKAGE_NAME=`cat package.json | jq -r .name`
 PUBLISHED_VERSION=`npm view ${PACKAGE_NAME} version`
 
-npm version --no-git-tag-version $PUBLISHED_VERSION
+npm version --no-git-tag-version --allow-same-version $PUBLISHED_VERSION
 
 PUBLISHED_SHA=`npm view ${PACKAGE_NAME} --json | jq .dist.shasum`
 NEW_SHA=`npm publish --dry-run --json | jq .shasum`
@@ -24,8 +24,8 @@ if [ "$PUBLISHED_SHA" != "$NEW_SHA" ]; then
 
     echo Publish: Publishing ${NEW_VERSION}
 
-    echo registry=http://registry.npmjs.org > .npmrc
-    echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} >> .npmrc
+    # echo registry=http://registry.npmjs.org > .npmrc
+    # echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} >> .npmrc
 
     npm publish
 else
